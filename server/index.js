502bb8782f8d68d82067985a226afaf315c306d7
Jeoffrey <jeoff.howe@gmail.com>
2019-04-23 15:05:52 -0600

    step 10

---
+++ server/index.js
@@ -0,0 +1,32 @@
+require('dotenv').config()
+
+const express = require('express')
+const session = require('express-session')
+const checkForSession = require('./middlewares/checkForSessions')
+const swagCtrl = require('./controllers/swagController')
+const authCtrl = require('./controllers/authController')
+const cartCtrl = require('./controllers/cartController')
+
+const app = express()
+const {SERVER_PORT,SECRET_SESSIONS} = process.env
+
+
+
+app.use(express.json())
+app.use(session({
+  secret: SECRET_SESSIONS,
+  resave:false,
+  saveUninitialized: true}))
+  app.use(checkForSession);
+
+app.get('/api/swag',swagCtrl.read)
+app.post('/api/login',authCtrl.login)
+app.post('/api/register', authCtrl.register)
+app.post('/api/signout', authCtrl.signout)
+app.get('/api/user', authCtrl.getUser)
+
+app.post('/api/cart/checkout',cartCtrl.checkout)
+app.post('/api/cart/:id', cartCtrl.add)
+app.delete('/api/cart/:id',cartCtrl.delete)
+
+app.listen(SERVER_PORT, () => console.log('listing on port', SERVER_PORT) )
